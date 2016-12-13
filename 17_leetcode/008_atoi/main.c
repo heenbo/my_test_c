22f87336b597342ec3e2693a65853a2545ca46a5
heenbo <379667345@qq.com>
2016-12-13 20:18:18 +0800

    负负得正处理，这才是对的，他们的 OJ 有问题

--- 17_leetcode/008_atoi/main.c
+++ 17_leetcode/008_atoi/main.c
@@ -3,12 +3,13 @@
  *   > Author: heenbo
  *   > Mail: 379667345@qq.com 
  *   > Created Time:  2016年12月13日 星期二 15时29分32秒
- *   > Modified Time: 2016年12月13日 星期二 16时28分56秒
+ *   > Modified Time: 2016年12月13日 星期二 20时14分30秒
  ************************************************************************/
 
 #include <stdio.h>
 
-#define MY_STR "-234214"
+#define MY_STR "+--1234567"
+//#define MY_STR "1"
 
 extern int myAtoi(char* str);
 
@@ -27,34 +28,47 @@ int main(int argc, char * argv[])
 int myAtoi(char * str)
 {
 	int result = 0;
-	int str_len = 0;
-	int i = 0;	
-	int j = 0;
 	int exp = 0;
+	int i = 0;
+	int j = 0;
+	int k = 0;
+	int symbol = 0;
 
-	while('\0' != str[i])
+	while('\0' != *str)
 	{
+		if('-' == *str)
+		{
+			symbol++;
+		}
+		str++;
 		i++;
 	}
-	str_len = i;
-	i--;
 	
-	for(;i >= 0; i--)
-	{
-		if('-' == str[i])
-		{
-			return (0-result);
-		}
-		else if('+' == str[i])
-		{
-			return result;
-		}
+	str--;
+	i--;
 
-		for(j = 0, exp = 1; j < str_len-i-1; j++)
+	printf("i = %d, *str = %c\n", i, *str);
+
+	while(('-' != *str) && ('+' != *str) && (0 <= i))
+	{
+		for(k = 0, exp = 1; k < j; k++)
 		{
 			exp *= 10;	
 		}
-		result += ((str[i]-'0')*exp);
+		printf("exp = %d\n", exp);
+		result += ((*str-'0')*exp);
+
+		str--;
+		i--;
+		j++;
 	}
-	return result;
+
+	if(symbol%2)
+	{
+		return (0-result);
+	}
+	else
+	{
+		return (result);
+	}
 }
