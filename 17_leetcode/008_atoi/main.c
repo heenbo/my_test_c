fcf4f27ed22849817e6ba83389f6d53914816104
heenbo <379667345@qq.com>
2016-12-14 13:33:27 +0800

    008 String to Integer (atoi) 终于 accept 了

--- 17_leetcode/008_atoi/main.c
+++ 17_leetcode/008_atoi/main.c
@@ -2,14 +2,25 @@
  *   > File Name: main.c
  *   > Author: heenbo
  *   > Mail: 379667345@qq.com 
- *   > Created Time:  2016年12月13日 星期二 15时29分32秒
- *   > Modified Time: 2016年12月13日 星期二 20时14分30秒
+ *   > Created Time:  2016年12月13日 星期二 19时29分32秒
+ *   > Modified Time: 2016年12月14日 星期三 13时30分31秒
  ************************************************************************/
 
 #include <stdio.h>
 
-#define MY_STR "+--1234567"
+//#define MY_STR "   +1234a567"
+//#define MY_STR "   +-1234a567"
 //#define MY_STR "1"
+//#define MY_STR "2147483648"
+//#define MY_STR "-2147483649"
+//#define MY_STR "   - 321"
+//#define MY_STR " b11228552307"
+//#define MY_STR "   010"
+//#define MY_STR "9223372036854775809"
+//#define MY_STR "-9223372036854775809"
+#define MY_STR "18446744073709551617"
+#define INT_MAX 2147483647
+#define INT_MIN -2147483648
 
 extern int myAtoi(char* str);
 
@@ -27,48 +38,91 @@ int main(int argc, char * argv[])
 
 int myAtoi(char * str)
 {
-	int result = 0;
-	int exp = 0;
+	long long result = 0;
 	int i = 0;
 	int j = 0;
-	int k = 0;
-	int symbol = 0;
+	long long exp = 0;
+	int symbol_count = 0;
 
-	while('\0' != *str)
+	while((!(((*str) - '0' <= 9) && ((*str) - '0' >= 0))) && (*str != '\0'))
 	{
-		if('-' == *str)
+		//if((*str == '-') || (*str == '+'))
+		if((!(((*str) - '0' <= 9) && ((*str) - '0' >= 0))) && (*str != ' '))
 		{
-			symbol++;
+			if((*str != '-') && (*str != '+'))
+			{
+				return 0;
+			}
+			symbol_count ++;
+			if(symbol_count > 1)
+			{
+				printf("line:%d, result= %lld\n", __LINE__, result);
+				return 0;
+			}
+			if(!(((str[1]) - '0' <= 9) && ((str[1]) - '0' >= 0)))
+			{
+				printf("line:%d, result= %lld\n", __LINE__, result);
+				return 0;
+			}
 		}
 		str++;
+	}
+
+	while((((*str) - '0' <= 9) && ((*str) - '0' >= 0)))
+	{
+		str++;
+		j++;
+		if(j > 10)
+		{
+			break;
+		}
+	}
+
+	str--;
+	while((((*str) - '0' <= 9) && ((*str) - '0' >= 0)))
+	{
+		for(j = 0, exp = 1; j < i; j++)
+		{
+			exp *= 10;
+		}
+		result += ((*str-'0')*exp);
+		str--;
 		i++;
 	}
-	
-	str--;
-	i--;
 
-	printf("i = %d, *str = %c\n", i, *str);
-
-	while(('-' != *str) && ('+' != *str) && (0 <= i))
+	printf("result= %lld\n", result);
+	if('-' == *str)
 	{
-		for(k = 0, exp = 1; k < j; k++)
+		if(result < 0)
 		{
-			exp *= 10;	
+			result = 0 - result;
 		}
-		printf("exp = %d\n", exp);
-		result += ((*str-'0')*exp);
-
-		str--;
-		i--;
-		j++;
-	}
-
-	if(symbol%2)
-	{
-		return (0-result);
+		printf("line:%d, result= %lld\n", __LINE__, result);
+		if((0 - result) < INT_MIN)
+		{
+			printf("line:%d, result= %lld\n", __LINE__, result);
+			return INT_MIN;
+		}
+		else
+		{
+			printf("line:%d, result= %lld\n", __LINE__, result);
+			return (0 - result);
+		}
 	}
 	else
 	{
-		return (result);
+		if(result < 0)
+		{
+			result = 0 - result;
+		}
+		printf("line:%d, result= %lld\n", __LINE__, result);
+		if(result > INT_MAX)
+		{
+			return INT_MAX;
+		}
+		else
+		{
+			return result;
+		}
 	}
 }
